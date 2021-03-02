4d0e627767dcee8d8c397b1e7a2b6aba2f22439e
MukarramAli26 <mukarramaliofficial@gmail.com>
2021-03-02 16:36:12 +0500

    project completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: ["gatsby-plugin-typescript",
+  `gatsby-plugin-material-ui`,
+  {
+    resolve: "gatsby-source-graphql",
+    options: {
+      // This type will contain remote schema Query type
+      typeName: "GCMS",
+      // This is field under which it's accessible
+      fieldName: "get_lollies",
+      // Url to query from
+      url:
+        "https://virtual-lolly-by-mateen.netlify.app/.netlify/functions/lollies",
+    },
+  },],
 }
